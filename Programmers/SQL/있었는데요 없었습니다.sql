95bffe9be10f52ed321e20f4be7f960bf27876fc
Seunghyun Shin <57430754+seunghyunshin111@users.noreply.github.com>
2020-12-25 03:41:28 +0900

    201210

--- Programmers/SQL/있었는데요 없었습니다.sql
+++ Programmers/SQL/있었는데요 없었습니다.sql
@@ -1,2 +1,4 @@
-SELECT max(datetime)
-from animal_ins
+SELECT ins.animal_id, ins.name
+from animal_ins ins, animal_outs outs
+WHERE ins.animal_id = outs.animal_id AND ins.datetime > outs.DATETIME
+order by ins.datetime
